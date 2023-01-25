11b32a2230cd460a9236a3be1e4843d9ac49b496
yavuzaydn <yavuz5680@gmail.com>
2023-01-26 00:37:15 +0300

    :q!

---
+++ playground/some.js
@@ -0,0 +1,19 @@
+const name = 'wes';
+const middle = 'topher';
+const last = 'bos';
+
+const html = `<div> I am ${1+100} years old`
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+console.log(`Each kids gets ${eachKidGets}`);
+
+const person = {
+    first: 'wes',
+    last: 'bos',
+    age: 100,
+};
+
+let somethingUndefined;
+const somethingNull = null;
